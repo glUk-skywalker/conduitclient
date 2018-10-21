c3a6b3e337c29abc3f0f9b0df58554adf7196565
Oleg Ivanov <ivanov@megaputer.ru>
2018-10-21 22:10:15 +0300

    Code style and incapsulation changes
    - `Client`: is now not exported `client`
    - Exported methods are at the top

--- client.go
+++ client.go
@@ -12,22 +12,55 @@ import (
 	"github.com/gluk-skywalker/conduitclient/responses"
 )
 
-// New creates and instance of Client
-func New(path string, token string) Client {
-	return Client{url: path, token: token}
+// New creates and instance of client
+func New(path string, token string) client {
+	return client{url: path, token: token}
 }
 
-// Client is object for interaction with the Phabricator conduit API
-type Client struct {
+// UserWhoAmI performs the `service.whoami` request
+func (c client) UserWhoAmI() (responses.UserWhoAmI, error) {
+	var userData responses.UserWhoAmI
+
+	basicResp, err := c.request("user.whoami", parameters.UserWhoAmI{})
+	if err != nil {
+		return userData, errors.New("whoami rquest error: " + err.Error())
+	}
+
+	err = json.Unmarshal(basicResp, &userData)
+	if err != nil {
+		return userData, errors.New("response parsing error: " + err.Error())
+	}
+	return userData, nil
+}
+
+// ProjectSearch performs the `project.search` request
+func (c client) ProjectSearch(params parameters.ProjectSearch) (responses.ProjectSearch, error) {
+	var projectData responses.ProjectSearch
+
+	basicResp, err := c.request("project.search", params)
+	if err != nil {
+		return projectData, errors.New("project.search rquest error: " + err.Error())
+	}
+
+	err = json.Unmarshal(basicResp, &projectData)
+	if err != nil {
+		return projectData, errors.New("response parsing error: " + err.Error())
+	}
+
+	return projectData, nil
+}
+
+// client is object for interaction with the Phabricator conduit API
+type client struct {
 	url   string
 	token string
 }
 
-func (c Client) generateURL(conduitMethod string, params url.Values) string {
+func (c client) generateURL(conduitMethod string, params url.Values) string {
 	return c.url + "/api/" + conduitMethod + "?" + params.Encode()
 }
 
-func (c Client) request(path string, params interface{ ToConduitParams() url.Values }) (json.RawMessage, error) {
+func (c client) request(path string, params interface{ ToConduitParams() url.Values }) (json.RawMessage, error) {
 	urlParams := params.ToConduitParams()
 	urlParams.Set("api.token", c.token)
 
@@ -57,37 +90,4 @@ func (c Client) request(path string, params interface{ ToConduitParams() url.Val
 	}
 
 	return conduitResp.Result, nil
-}
-
-// UserWhoAmI performs the `service.whoami` request
-func (c Client) UserWhoAmI() (responses.UserWhoAmI, error) {
-	var userData responses.UserWhoAmI
-
-	basicResp, err := c.request("user.whoami", parameters.UserWhoAmI{})
-	if err != nil {
-		return userData, errors.New("whoami rquest error: " + err.Error())
-	}
-
-	err = json.Unmarshal(basicResp, &userData)
-	if err != nil {
-		return userData, errors.New("response parsing error: " + err.Error())
-	}
-	return userData, nil
-}
-
-// ProjectSearch performs the `project.search` request
-func (c Client) ProjectSearch(params parameters.ProjectSearch) (responses.ProjectSearch, error) {
-	var projectData responses.ProjectSearch
-
-	basicResp, err := c.request("project.search", params)
-	if err != nil {
-		return projectData, errors.New("project.search rquest error: " + err.Error())
-	}
-
-	err = json.Unmarshal(basicResp, &projectData)
-	if err != nil {
-		return projectData, errors.New("response parsing error: " + err.Error())
-	}
-
-	return projectData, nil
 }
