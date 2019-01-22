1791858d7f7b9d2937dd6be65308d1445ea3cf56
Sergey Bykov <sbykov@mail.megaputer.ru>
2019-01-22 16:48:42 +0300

    Fix PaginationCursor

--- serviceobjects/paginationcursor.go
+++ serviceobjects/paginationcursor.go
@@ -2,8 +2,8 @@ package serviceobjects
 
 // PaginationCursor is the structure for data stored under `cursor` key in some responses
 type PaginationCursor struct {
-	Limit  int    `json:"limit"`
-	After  int    `json:"after"`
-	Before int    `json:"before"`
-	Order  string `json:"order"`
+	Limit  int      `json:"limit"`
+	After  string   `json:"after"`
+	Before string   `json:"before"`
+	Order  []string `json:"order"`
 }
