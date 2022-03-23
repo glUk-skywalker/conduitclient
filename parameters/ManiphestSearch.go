582c9c8f2f39fe2beb078626435f5bd9b697e808
Anna Vasileva <avasileva@megaputer.ru>
2022-03-23 10:39:10 +0300

    Updated some structs

--- parameters/ManiphestSearch.go
+++ parameters/ManiphestSearch.go
@@ -8,9 +8,12 @@ import (
 // ManiphestSearch is the structure for the params of `maniphest.search` query
 type ManiphestSearch struct {
 	Constraints struct {
-		IDs      []int
-		Statuses []string
-		Projects []string
+		IDs           []int
+		Statuses      []string
+		Projects      []string
+		Priorities    []int
+		ModifiedStart int64
+		ModifiedEnd   int64
 	}
 	Order       []string
 	After       string
@@ -23,21 +26,39 @@ func (p ManiphestSearch) ToConduitParams() url.Values {
 	for i, v := range p.Constraints.IDs {
 		params.Add("constraints[ids]["+strconv.Itoa(i)+"]", strconv.Itoa(v))
 	}
+
 	for i, v := range p.Constraints.Statuses {
 		params.Add("constraints[statuses]["+strconv.Itoa(i)+"]", v)
 	}
+
 	for i, v := range p.Constraints.Projects {
 		params.Add("constraints[projects]["+strconv.Itoa(i)+"]", v)
 	}
+
+	for i, v := range p.Constraints.Priorities {
+		params.Add("constraints[priorities]["+strconv.Itoa(i)+"]", strconv.Itoa(v))
+	}
+
+	if p.Constraints.ModifiedStart != 0 {
+		params.Add("constraints[modifiedStart]", strconv.FormatInt(p.Constraints.ModifiedStart, 10))
+	}
+
+	if p.Constraints.ModifiedEnd != 0 {
+		params.Add("constraints[modifiedEnd]", strconv.FormatInt(p.Constraints.ModifiedStart, 10))
+	}
+
 	for i, v := range p.Order {
 		params.Add("order["+strconv.Itoa(i)+"]", v)
 	}
+
 	if p.After != "" {
 		params.Add("after", p.After)
 	}
+
 	for key, v := range p.Attachments {
 		params.Add("attachments["+key+"]", btos(v))
 	}
+
 	return params
 }
 
